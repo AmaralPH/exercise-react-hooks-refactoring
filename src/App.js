47d97469b2bf67d81f6f019bbc06e12e13fcacc7
Pedro Amaral <amaralph1995@gmail.com>
2021-04-15 19:10:21 -0300

    Conclui exercício 2

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import React from 'react';
 import './App.css';
 import Cars from './Cars';
 import TrafficSignal from './TrafficSignal';
+import Provider from './context/Provider';
 
 function App() {
   return (
     <div className="container">
-      <Cars />
-      <TrafficSignal />
+      <Provider>
+        <Cars />
+        <TrafficSignal />
+      </Provider>
     </div>
   );
 }
